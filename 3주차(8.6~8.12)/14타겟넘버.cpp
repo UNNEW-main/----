f2d343aa4d3c1edc1c373e67a0f66f8a1fc050f4
UNNEW-main <201710064c@gmail.com>
2021-08-06 16:54:15 +0900

    14번문제풀이

---
+++ 3주차(8.6~8.12)/14타겟넘버.cpp
@@ -0,0 +1,25 @@
+//https://programmers.co.kr/learn/courses/30/lessons/43165
+//DFS 개념부족, 참고
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void dfs(vector<int>numbers, int target, int &answer, int count=0, int sum=0){
+    if(count == numbers.size()){
+        if(sum == target)
+            answer++;
+        return;
+    }
+    
+    dfs(numbers, target, answer, count+1, sum+numbers[count]);
+    dfs(numbers,target, answer, count+1, sum-numbers[count]);
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    dfs(numbers, target, answer);
+    
+    return answer;
+}
