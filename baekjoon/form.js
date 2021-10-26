3f7703211b4964ec9ead1a80be229ded7304d31f
UNNEW-main <1970005826c@gmail.com>
2021-10-26 21:41:54 +0900

    3번문제풀이

--- baekjoon/form.js
+++ baekjoon/form.js
@@ -2,8 +2,8 @@
 //node baekjoon/1주차/test.js (현재경로)
 
 //////////////한 줄에 한 단어 입력
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
 
 let num = Number(input);
 
@@ -12,8 +12,8 @@ for (let i = 1; i <= num; i++) {
 }
 
 /////////////한 줄에 여러 단어 입력
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 let num = [];
 for (let i = 0; i < input.length; ++i) {
   num.push(Number(input[i]));
@@ -21,8 +21,8 @@ for (let i = 0; i < input.length; ++i) {
 console.log(num);
 
 //////////////여러 줄 입력
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 let count = input[0]; //맨 앞 입력값, 몇개를 입력받을 거냐
 let numbers = []; //input값을 받을 변수
