b11883fc555a44e60e9a5c636b761caa48baf86c
UNNEW-main <1970005826c@gmail.com>
2021-10-26 21:41:43 +0900

    3문제풀이

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -1,19 +1,32 @@
-//https://www.acmicpc.net/problem/18228
+//https://www.acmicpc.net/problem/1520
 
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let arr = input[1].split(' ').map(Number);
-
-let temp = arr[0];
-let result = 0;
-
-arr.forEach((item) => {
-  if (item === -1) {
-    result += temp;
-    temp = arr[arr.length - 1];
-    return;
+let size = input[0].split(' ').map(Number); //세로 , 가로
+let rooms = [];
+let xx = [1, -1, 0, 0];
+let yy = [0, 0, 1, -1];
+let v = Array.from(Array(size[0]), () => Array(size[1]).fill(-1)); //방문한 값 저장
+//값 입력받기
+for (let i = 1; i < input.length; i++) {
+  if (input[i] !== '') {
+    rooms.push(input[i].split(' ').map(Number));
   }
-  if (item < temp) temp = item;
-});
-result += temp;
+}
+
+let result = dps(0, 0);
 console.log(result);
+
+function dps(x, y) {
+  if (x === size[1] - 1 && y === size[0] - 1) return 1;
+  if (v[y][x] !== -1) return v[y][x];
+  v[y][x] = 0;
+  for (let i = 0; i < 4; ++i) {
+    let nextX = x + xx[i];
+    let nextY = y + yy[i];
+    if (nextX >= 0 && nextY >= 0 && nextX < size[1] && nextY < size[0] && rooms[y][x] > rooms[nextY][nextX]) {
+      v[y][x] += dps(nextX, nextY);
+    }
+  }
+  return v[y][x];
+}
