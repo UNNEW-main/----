5646f4e75ab2a101d3345960b21aef665984953c
UNNEW-main <1970005826c@gmail.com>
2021-12-09 18:38:45 +0900

    [ADD] 15문제풀이

--- baekjoon/form.js
+++ baekjoon/form.js
@@ -26,9 +26,16 @@ let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n
 let [count, ...numbers] = input;
 numbers = numbers.map((i) => Number(i));
 
+//처리방법1
 for (let i = 1; i < input.length; i++) {
   //맨 앞에 count를 입력받았으니 index 1부터 시작
   if (input[i] !== '') {
     numbers.push(input[i].split(' '));
   }
+}
+//처리방법2
+for (let i = 0; i < count; ++i) {
+  arr[i] = new Array();
+  arr[i][0] = numbers[i * 2].split(' ').map((item) => Number(item));
+  arr[i][1] = numbers[i * 2 + 1].split(' ').map((item) => Number(item));
 }
