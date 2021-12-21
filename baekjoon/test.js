7fa5b9d76df355444612d2604578afef207697c7
UNNEW-main <1970005826c@gmail.com>
2021-12-21 21:44:38 +0900

    [ADD] 16문제풀이

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -1,57 +1,16 @@
-//https://www.acmicpc.net/problem/1966
-//전에 풀었던 우선수위 프린터 큐
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-let [count, ...numbers] = input;
-let arr = [];
+//https://www.acmicpc.net/problem/1541
+//접근은 쉬웠지만 계산이 귀찮
 
-for (let i = 0; i < count; ++i) {
-  arr[i] = new Array();
-  arr[i][0] = numbers[i * 2].split(' ').map((item) => Number(item));
-  arr[i][1] = numbers[i * 2 + 1].split(' ').map((item, index) => {
-    if (index === arr[i][0][1]) {
-      return (item = [Number(item), 99]);
-    } else {
-      return (item = [Number(item), 0]);
-    }
-  });
-}
-for (let i = 0; i < count; ++i) {
-  Priority_Queue(arr[i][1]);
-}
-// [
-//   [[1, 0], [5]],
-//   [
-//     [4, 2],
-//     [1, 2, 3, 4],
-//   ],
-//   [
-//     [6, 0],
-//     [1, 1, 9, 1, 1, 1],
-//   ],
-// ];
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('-');
+let result = 0;
+for (let i = 0; i < input.length; ++i) {
+  let temp = 0;
 
-function Priority_Queue(arrProps) {
-  let arrTemp = arrProps;
-  let result = 0;
-  let arrLength = arrProps.length;
-  while (arrLength !== 0) {
-    let temp = arrTemp[0];
-    let check = 0;
-    arrTemp.shift();
-    arrLength--;
-    for (let i = 0; i < arrLength; ++i) {
-      if (temp[0] < arrTemp[i][0]) {
-        arrTemp.push(temp);
-        arrLength++;
-        check = 1;
-        break;
-      }
-    }
-    if (check === 0) {
-      result++;
-      if (temp[1] === 99) {
-        return console.log(result);
-      }
-    }
+  let noPlus = input[i].split('+');
+  for (let j = 0; j < noPlus.length; ++j) {
+    temp += Number(noPlus[j]);
   }
+  if (i === 0) result += temp;
+  else result -= temp;
 }
+console.log(result);
