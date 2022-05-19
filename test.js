5d07d2d5fbe7e2f2949159e6ed789e96d0c55db7
UNNEW-main <1970005826c@gmail.com>
2022-05-19 09:37:57 +0900

    feat: l4 MissingInterger 문제풀이

--- test.js
+++ test.js
@@ -1,24 +1,23 @@
-const N = 5;
-const A = [6, 6, 6, 6, 6, 6];
-// const N = 5;
-// const A = [3, 4, 4, 6, 1, 4, 4];
-//[3, 2, 2, 4, 2]
-
-//5, [6, 6, 6, 6, 6, 6] => [0, 0, 0, 0, 0]
-const arr = Array.from({ length: N }, () => 0);
-let max = 0;
-for (let i = 0; i < A.length; ++i) {
-  if (A[i] === N + 1) {
-    max = Math.max(...arr);
-  } else if (max > arr[A[i] - 1]) {
-    arr[A[i] - 1] = max + 1;
-  } else {
-    arr[A[i] - 1]++;
-  }
-}
-arr.map((data) => (data > max ? data : max));
-
-console.log(arr);
+// a = [1, 3, 6, 4, 1, 2];
 
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
+
+const A = Array.from({ length: 100 });
+for (let i = 0; i < A.length; ++i) {
+  A[i] = i;
+}
+console.log(A);
+// function solution(A) {
+// write your code in JavaScript (Node.js 8.9.4)
+let result = 1;
+A.sort((a, b) => a - b);
+for (let i = 0; i < A.length; ++i) {
+  console.log(A[i]);
+  if (A[i] === result) {
+    console.log(result);
+    result++;
+  }
+}
+return result;
+// }
