5ef7874f32b417eb4558d0b604487a7af828bfb5
UNNEW-main <1970005826c@gmail.com>
2022-08-12 17:13:49 +0900

    [ADD] L6 문제풀이

--- codility/L6/NumberOfDiscIntersections.js
+++ codility/L6/NumberOfDiscIntersections.js
@@ -1,4 +1,11 @@
 //https://app.codility.com/programmers/lessons/6-sorting/number_of_disc_intersections/
+//81% .. timeout 으로 결국 100퍼를 채우지 못했다.
+
+// 원의 낮은 값들을 lower배열에 정렬
+// 원의 높은 값들을 upper배열에 정렬
+
+// lower의 인덱스값은 해당 원이 겹칠 수 있는 최댓값이다.
+// 그 최댓값에서 upper배열을 보면서 lower값보다 낮은 값만 빼면 해당원의 겹침갯수가 된다.
 
 const A = [1, 5, 2, 1, 4, 0];
 
